06564fc635c8ae265e39e8bba586ee072eb88eaa
soyunkim <ksy010627@yonsei.ac.kr>
2024-07-22 14:25:17 +0900

    modified merge.py

---
+++ merge.py
@@ -0,0 +1,26 @@
+# merge.py
+
+def greet():
+    print("Hello from the SOYUN's branch")
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+def main():
+    print("This is the main function")
+    greet()
+    result_add = add(10, 5)
+    result_sub = subtract(10, 5)
+    result_mul = multiply(10, 5)
+    result_div = divide(10, 5)
+    print(f"Add: {result_add}, Subtract: {result_sub}, Multiply: {result_mul}, Divide: {result_div}")
+          
+if __name__ == "__main__":
+    main()
